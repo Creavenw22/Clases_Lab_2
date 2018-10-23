a1c4ab7421d76a108e6da9e72a361def435b74a6
alumno <alumno>
2018-10-23 11:36:21 -0300

    clase 23/10 Deposito3 incompleto

---
+++ Clase 23-10/Deposito (parte 1)/Entidades/Auto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Auto
+    {
+        #region Atributos
+        private string _color;
+        private string _marca;
+        #endregion
+
+        #region Properties
+        public string Color
+        {
+            get { return this._color; }
+        }
+
+        public string Marca
+        {
+            get { return this._marca; }
+        }
+        #endregion
+
+        #region Constructor
+        public Auto(string color, string marca)
+        {
+            this._marca = marca;
+            this._color = color;
+        }
+        #endregion
+
+        #region Operadores
+        public static bool operator ==(Auto a1, Auto a2)
+        {
+            bool flag = false;
+
+            if (a1.Marca == a2.Marca && a1.Color == a2.Color)
+            {
+                flag = true;
+            }
+
+            return flag;
+        }
+
+        public static bool operator !=(Auto a1, Auto a2)
+        {
+            return !(a1 == a2);
+        }
+        #endregion
+
+        #region Sobrecargas
+        public override bool Equals(object obj)
+        {
+            return (this==(Auto)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Marca: {0} \nColor: {1}\n",this.Marca,this.Color);
+
+            return sb.ToString();
+        }
+        #endregion
+        
+    }
+}
---
+++ Clase 23-10/Deposito (parte 2)/EntidadesTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace EntidadesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\##Bustamante\Clases_Lab_2\Clase 23-10 (parte 2)\Cocinas.txt";
+            Cocina c1 = new Cocina(111, 12300, false);
+            Cocina c2 = new Cocina(112, 15000, true);
+            Cocina c3 = new Cocina(113, 5600, false);
+            DepositoDeCocinas dc = new DepositoDeCocinas(5);
+            dc.Agregar(c1);
+            dc.Agregar(c2);
+            if (!(dc + c3))
+            {
+                Console.WriteLine("No se pudo agregar el item!!!");
+            }
+            Console.WriteLine(dc);
+            dc.Remover(c2);
+            if (!(dc - c2))
+            {
+                Console.WriteLine("No se pudo remover el item!!!");
+            }
+            Console.WriteLine(dc);
+            
+
+            if (dc.Guardar(path))
+            {
+                Console.WriteLine("Se pudo guardar el archivo");
+            }
+
+            else
+            {
+                Console.WriteLine("No se pudo guardar el archivo");
+            }
+
+            if (dc.Recuperar(path))
+            {
+                Console.WriteLine("Se pudo recuperar el archivo");
+            }
+
+            else
+            {
+                Console.WriteLine("No se pudo recuperar el archivo");
+            }
+            Console.ReadLine();
+        }
+    }
+}
