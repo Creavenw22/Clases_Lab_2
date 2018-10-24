2c3234a4f5fc555d4c258b9e977d0f574157ef15
alumno <alumno>
2018-10-24 12:01:46 -0300

    Implementar las interfaces y realizar la segunda parte

--- Clases Interfaces/Interfaces (parte 1)/Entidades/Vehiculo.cs
+++ Clases Interfaces/Interfaces (parte 1)/Entidades/Vehiculo.cs
@@ -8,16 +8,30 @@ namespace Entidades
 {
     public abstract class Vehiculo
     {
+        #region Atributos
         protected double _precio;
+        #endregion
 
+        #region Properties
+        public double Precio
+        {
+            get { return this._precio; }
+            set { this._precio = value; }
+        }
+        #endregion
+
+        #region Metodos
         public void MostrarPrecio()
         {
-
+            Console.WriteLine("Precio: {0}", this.Precio);
         }
+        #endregion
 
+        #region Constructor
         public Vehiculo(double precio)
         {
             this._precio = precio;
         }
+        #endregion
     }
 }
