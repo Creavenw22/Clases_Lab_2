fd661f077474736e11e1c0723e69d367548c26e9
alumno <alumno>
2018-10-30 10:14:24 -0300

    Added UnitTest && Archivos

---
+++ Clases Archivos/AdministradorArchivos/AdministradorArchivos.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Entidades
+{
+    public static class AdministradorArchivos
+    {
+        
+        #region Metodos
+        public static bool Escribir(string path, string texto)
+        {
+            bool flag=false;
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.Write(texto.ToString());
+                    flag = true;
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return flag;
+        }
+
+        public static bool Leer(string path, out string texto)
+        {
+            bool flag=false;
+            texto = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    texto = sr.ReadToEnd();
+                    flag = true;
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return flag;
+        }
+        #endregion
+
+    }
+    
+}
---
+++ Clases Archivos/TestArchivos/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace TestArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\##Bustamante\Clases_Lab_2\Clases Archivos\AdministradorArchivos\texto.txt";
+            string escritorio = @"C:\Users\alumno\Desktop\TestDesk.txt";
+            string misDocumentos = @"C:\Users\alumno\Documents\testDocs.txt";
+            string misImagenes = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\testPics.txt";//@"C:\Users\alumno\Pictures\testPics.txt";
+            string texto1,texto2,texto3,texto4;
+
+
+            if (!(AdministradorArchivos.Escribir(path, "EAEAEAEA FUNCIONA PERREKE")))
+            {
+                Console.WriteLine("No se pudo escribir el archivo");
+            }
+            if (!(AdministradorArchivos.Escribir(escritorio, "Este es el archivo de escritorio")))
+            {
+                Console.WriteLine("No se pudo escribir el archivo");
+            }
+            if (!(AdministradorArchivos.Escribir(misDocumentos, "Este es el archivo de documentos")))
+            {
+                Console.WriteLine("No se pudo escribir el archivo");
+            }
+            if (!(AdministradorArchivos.Escribir(misImagenes, "Este es el archivo de imagenes")))
+            {
+                Console.WriteLine("No se pudo escribir el archivo");
+            }
+
+            if (!(AdministradorArchivos.Leer(path, out texto1)))
+            {
+                Console.WriteLine("No se pudo leer el archivo");
+            }
+            if (!(AdministradorArchivos.Leer(escritorio, out texto2)))
+            {
+                Console.WriteLine("No se pudo leer el archivo");
+            }
+            if (!(AdministradorArchivos.Leer(misDocumentos, out texto3)))
+            {
+                Console.WriteLine("No se pudo leer el archivo");
+            }
+            if (!(AdministradorArchivos.Leer(misImagenes, out texto4)))
+            {
+                Console.WriteLine("No se pudo leer el archivo");
+            }
+
+
+            Console.WriteLine(texto1);
+            Console.WriteLine(texto2);
+            Console.WriteLine(texto3);
+            Console.WriteLine(texto4);
+
+            Console.ReadLine();
+        }
+    }
+}
